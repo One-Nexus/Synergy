93e27b253e877db8ff8cd5231b8adf4ac6ea23d0
Edmund Reed <esr360@live.com>
2015-10-17 01:18:26 +0100

    get json config from dom

---
+++ src/modular.js
@@ -0,0 +1,14 @@
+/*-----------------------------------------------------------------
+
+Modular - JS Extension
+Made by @esr360
+http://github.com/esr360/Modular/
+	
+-----------------------------------------------------------------*/
+
+//-----------------------------------------------------------------
+// Convert CSS config to JS
+//-----------------------------------------------------------------
+
+// Get styles' configuration
+var stylesConfigJSON = document.getElementById("stylesConfigJSON");
