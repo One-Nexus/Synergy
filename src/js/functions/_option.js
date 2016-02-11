c5df30cfa3f04d3041cd5ef530b7d81898b80215
Edmund Reed <esr360@live.com>
2016-02-11 23:53:19 +0000

    separating code into modular files

---
+++ src/js/functions/_option.js
@@ -0,0 +1,27 @@
+/**
+ * @function _option
+ * @description Get the computed value of a module's option
+ * @param module - The module which you wish to get an option from
+ * @param option - The module's option which you are interested in
+ */
+
+//	Usage
+//
+//	if(_option('navigation', 'sticky')) {
+//		...
+//	}
+
+function _option(module, option) {
+
+    var $option   = _module[module][option];
+    var $value    = $option[Object.keys($option)[0]];
+    var $id       = $('.' + module + ', [class*="' + module + '-"]');
+    var $selector = $id.is('[class*="-' + option + '"]') == true;
+        
+    if (typeof($value) == 'boolean') {
+        return $selector || $option['enabled'] != false;
+    } else {
+        return $selector || $option != false;
+    }
+	
+}
---
+++ src/js/functions/isModifier.js
@@ -0,0 +1,23 @@
+/**
+ * @function $.isModifier
+ * @description Determine if a particular module has a certain 
+ * modifier. Will look for either the modifier on the module in the
+ * DOM or look for a corresponding value in the configuration.
+ * @param module - The module which you wish to get an option from
+ */
+
+//	Usage
+//
+//	if(_navigation.isModifier('sticky')) {
+//		...
+//	}
+
+$.fn.isModifier = function(modifier) {
+    
+    var moduleName = $(this)['selector'].split(',')[0].split('.').join('');
+    
+    moduleName = moduleName.toString();
+    
+    return _option(moduleName, modifier);
+    
+};
