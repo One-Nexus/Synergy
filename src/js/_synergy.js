0fbd79a031fed461cbfb4cf9c2f3d9e0bfb7c303
esr360 <esr360@live.com>
2017-08-18 20:00:10 +1000

    adding  author info to main files

---
+++ src/js/_synergy.js
@@ -0,0 +1,9 @@
+///****************************************************************
+/// Synergy - https://github.com/esr360/Synergy
+///
+/// @author [@esr360](http://twitter.com/esr360)
+///****************************************************************
+
+import Synergy from './synergy';
+
+window.Synergy = window.Synergy || Synergy;
