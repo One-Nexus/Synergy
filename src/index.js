2830d51a53d1c74f076a71095f9e9fb167ab8812
esr360 <esr360@live.com>
2018-03-27 20:08:45 +1100

    exporting wrapper and group

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 export { default as Synergy } from './js/synergy.js';
 export { default as Module } from './js/module.jsx';
 export { default as Component } from './js/component.jsx';
+export { Wrapper } from './js/component.jsx';
+export { Group } from './js/component.jsx';
