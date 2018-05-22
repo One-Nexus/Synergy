6a19f44a33e5b90535042b4b1d1320e559721eb7
esr360 <esr360@live.com>
2018-05-22 23:58:15 +1000

    adding function to Constructor to get Module content

--- src/js/module.jsx
+++ src/js/module.jsx
@@ -19,6 +19,30 @@ export class Constructor extends React.Component {
         this.methods.forEach(method => {
             this[method] = Synergy.modules[this.props.name].methods[method];
         });
+
+        this.content = defaults => {
+            if (this.props.content) {
+                return defaults;
+            }
+
+            if (this.containsStaticMethodContent(this.props.children)) {
+                return this.props.children;
+            }
+
+            return defaults;
+        };
+    }
+
+    containsStaticMethodContent(props) {
+        return Object.entries(this.props).some(prop => {
+            const [key, value] = [prop[0], prop[1]];
+
+            if (value.constructor === Array) {
+                return value.find(prop => prop.type === this.constructor.content);
+            } else {
+                return value.type === this.constructor.content;
+            }
+        });
     }
 }
 
