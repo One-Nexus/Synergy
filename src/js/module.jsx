0ea7d83726745aba59223d36cf20689b7e69430c
esr360 <esr360@live.com>
2018-07-26 00:35:16 +1000

    css-in-js stuff

--- src/js/module.jsx
+++ src/js/module.jsx
@@ -30,6 +30,38 @@ export default class Module extends React.Component {
         this.classNames = props.name + this.modifiers + this.classes;
         this.id = props.id;
 
+        this.ref = node => {
+            if (props.init) props.init(node);
+
+            if (props.styles) {
+                props.styles(node);
+
+                // Options for the observer (which mutations to observe)
+                const config = { attributes: true, childList: true, subtree: true };
+
+                // Callback function to execute when mutations are observed
+                const callback = function(mutationsList, observer) {
+                    for (var mutation of mutationsList) {
+
+                        console.log(mutation);
+
+                        if (mutation.type == 'childList') {
+                        }
+                        else if (mutation.type == 'attributes') {
+                            props.styles(node);
+                            observer.disconnect();
+                        }
+                    }
+                };
+
+                // Create an observer instance linked to the callback function
+                const observer = new MutationObserver(callback);
+
+                // Start observing the target node for configured mutations
+                observer.observe(node, config);
+            }
+        }
+
         increment++;
 
         if (props.component) this.tag = props.component;
@@ -66,9 +98,6 @@ export default class Module extends React.Component {
         });
     }
 
-    /**
-     * Content to pass to children components
-     */
     getChildContext() {
         let config;
 
@@ -131,6 +160,7 @@ export default class Module extends React.Component {
                     id={this.id}
                     className={this.classNames}
                     data-module={this.props.name}
+                    ref={this.ref}
 
                     {...getHtmlProps(this.props)}
                     {...this.getDataAttributes(this.props)}
@@ -144,6 +174,47 @@ export default class Module extends React.Component {
             </React.Fragment>
         );
     }
+}
+
+Module.setStyles = (element, styles, globals, theme, scope) => {
+    const moduleName = element.getAttribute('data-module');
+
+    for (let [key, value] of Object.entries(typeof styles === 'object' ? styles : styles(element, globals))) {
+
+        let namespace = `${scope || moduleName}_${key}`;
+
+        if (key.indexOf('modifier(') > -1) {
+            let modifier = key.replace('modifier(', '');
+
+            modifier = modifier.replace(/\)/g, '');
+
+            if (element.modifier(modifier)) {
+                Module.setStyles(element, value, globals, theme);
+            }
+
+            continue;
+        }
+
+        if (typeof value === 'object') {
+            let components = element.querySelectorAll(`.${namespace}`);
+
+            if (!components.length) {
+                components = element.component(key);
+            }
+
+            if (components.length) {
+                [...components].forEach(component => {
+                    Module.setStyles(component, value, globals, theme, namespace);
+                });
+            } else {
+                // console.log(components, element, namespace);
+            }
+        } else {
+            element.style[key] = value;
+        }
+    }
+
+    if (theme) Module.setStyles(element, theme, globals);
 }
 
 Module.childContextTypes = {
