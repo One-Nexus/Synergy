d63d38d9ca3add54ae345caa3ed94129e6f5d3e5
esr360 <esr360@live.com>
2018-11-15 00:36:41 +1100

    grooming

--- src/synergy.js
+++ src/synergy.js
@@ -1,18 +1,25 @@
 import * as lucid from '../../Lucid/src';
 import polymorph from '../../Polymorph/src/polymorph';
 import sQuery from '../../sQuery/src/squery';
+import deepextend from 'deep-extend';
 
 if (typeof window !== 'undefined') {
     sQuery.init();
 
     Object.assign(window, lucid);
 
-    window.Synergy.styleParser = polymorph;
+    Synergy.styleParser = polymorph;
 
-    window.Synergy.theme = (modules, theme) => {
+    Synergy.config = (...params) => deepextend({}, ...params);
+
+    Synergy.theme = (modules, ui, theme) => {
+        window.ui = Synergy.config(ui, theme);
+
+        delete window.ui.modules;
+
         Object.values(modules).forEach(MODULE => MODULE.defaults && (
             window[MODULE.name] = Object.assign(MODULE, {
-                config: Module.config(MODULE.defaults(window.theme), theme[MODULE.name])
+                config: Module.config(MODULE.defaults(ui), theme.modules[MODULE.name])
             })
         ));
     };
