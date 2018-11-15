7fc13ece56554729474a0aa67d7315f514fc90cd
esr360 <esr360@live.com>
2018-11-16 01:00:08 +1100

    adding synergy.app()

--- src/synergy.js
+++ src/synergy.js
@@ -2,6 +2,7 @@ import * as lucid from '../../Lucid/src';
 import polymorph from '../../Polymorph/src/polymorph';
 import sQuery from '../../sQuery/src/squery';
 import deepextend from 'deep-extend';
+import { HashRouter, Route } from 'react-router-dom';
 
 if (typeof window !== 'undefined') {
     sQuery.init();
@@ -17,21 +18,30 @@ if (typeof window !== 'undefined') {
 
         delete window.ui.modules;
 
+        Synergy.CssClassProps = theme['css-class-props'];
+
         Object.values(modules).forEach(MODULE => MODULE.defaults && (
             window[MODULE.name] = Object.assign(MODULE, {
                 config: Module.config(MODULE.defaults(ui), theme.modules[MODULE.name])
             })
         ));
     };
-}
 
-export {
-    Synergize,
-    Module, 
-    Wrapper, 
-    Group,
-    Component, 
-    SubComponent,
-    sQuery,
-    polymorph
+    Synergy.app = ({ modules, ui, theme, pages, config }) => {
+        Synergy.theme(modules, ui, theme);
+
+        return (
+            <HashRouter>
+                <React.Fragment>
+                    <Route path='/' exact render={() => <pages.index config={config} />} />
+    
+                    {Object.keys(pages).map((page, index) => <Route key={index} path={`/${page}`} render={() => {
+                        const Page = pages[page];
+    
+                        return <Page config={config} />;
+                    }} />)}
+                </React.Fragment>
+            </HashRouter>
+        );
+    }
 }
